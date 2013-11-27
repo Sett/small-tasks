f190a5daab33101ac415bf1cb70c7e770cbe94ce
Se# <support@funcraft.ru>
2013-11-27 14:10:11 +0300

    Create onload.php

---
+++ dancedown-traits/application/configs/onload.php
@@ -0,0 +1,19 @@
+<?php
+
+return 
+[
+    "talk" => "on",
+
+    "system" =>
+    [
+        "title" => "Dance Club",
+        "version" => "0.0.start.1"
+    ],
+
+    "event" =>
+    [
+        "club opened"   => ["clubOpened"],
+        "music on"      => ["play"],
+        "the end"       => ["output"]
+    ]
+];
