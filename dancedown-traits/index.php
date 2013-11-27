cc93bbeff4372050ae6414bdb70220658fbec0fe
Se# <support@funcraft.ru>
2013-11-27 15:17:00 +0300

    Update index.php

--- dancedown-traits/index.php
+++ dancedown-traits/index.php
@@ -8,14 +8,6 @@ define('DC_ROOT', __DIR__ . '/');
 require_once 'Dance_Club.php';
 $start = time();
 
-$music = ['rock', 'pop'];
-$names = ['paul', 'marry'];
-
-$people = [];
-$amount = mt_rand(0, 20);
-for($i = 0; $i < $amount; $i++)
-  $people[$names[mt_rand(0, 1)]] = $music[mt_rand(0,1)];
-
 $dc = new Dance_Club($people, __DIR__ . '/application/configs/onload.php');
 
 $dc->output($dc->say('Finish in ' . (time()-$start) . ' second(s)', 'h1', true));
