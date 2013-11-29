401bf3e1a9b7edbcc7bce328852a49e0bbfc7638
Se# <support@funcraft.ru>
2013-11-29 12:17:44 +0300

    Update People.php

--- dancedown-traits/modules/People.php
+++ dancedown-traits/modules/People.php
@@ -6,7 +6,10 @@ trait People
     {
         $people = [];
         for($i = 0; $i < $count; $i++)
-            $people[$this->names[mt_rand(0, $this->namesCount)]] = $this->music['data'][mt_rand(0,$this->musicCount)]['name'];
+        {
+            $people[$this->names[mt_rand(0, $this->namesCount)]] = 
+            $this->music['data'][mt_rand(0,$this->musicCount)]['name'];
+        }
         
         return $people;
     }
