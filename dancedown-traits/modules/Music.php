df3d6a634126ae0392e2bc973baf89fc57b47c34
Se# <support@funcraft.ru>
2013-11-29 12:17:04 +0300

    Update Music.php

--- dancedown-traits/modules/Music.php
+++ dancedown-traits/modules/Music.php
@@ -2,8 +2,21 @@
 
 trait Music
 {
-    public function play($music = '')
+    public $playing = 'rock';
+    
+    public function play($people = [])
     {
+        $this->say('Playing ' . $this->playing, 'h1');
         
+        foreach($people as $name => $music)
+        {
+            if($music == $this->playing)
+            {
+                $index = $this->music['map'][$music];
+                $this->say($name . ' ' . $this->music['data'][$index]['dance']);
+            }
+            else
+                $this->say($name . ' goes drinking');
+        }
     }
 }
