e87a09b0259cd854ae136723674c3d4bfe84a223
Se# <support@funcraft.ru>
2013-11-29 12:05:16 +0300

    Update DanceClub.php

--- dancedown-traits/modules/DanceClub.php
+++ dancedown-traits/modules/DanceClub.php
@@ -6,10 +6,11 @@ trait DanceClub
      * @param array $people
      * @param string $onLoadConfig path/to/file
      */
-    public function __construct($onLoadConfig = '', $amount = 20)
+    public function __construct($onLoadConfig = '', $amount = 20, $music = '')
     {
         $config = $onLoadConfig ? $onLoadConfig : __DIR__ . '/../application/configs/onload.json';
 
+        $this->music = $music ? $music : $this->music;
         $this->applyConfig($config);
         $this->run($this->getPeople(20));
     }
